98afe0b2d0b8467221752a5900f475c0db71a2f1
Lina Ebeid <linaebe0@gmail.com>
2020-08-29 13:55:25 +0300

    initialize pages

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1 className="red">Hi About</h1>
+  </Layout>
+)
+
+export default AboutPage;
+
---
+++ src/pages/project.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ProjectPage = () => (
+  <Layout>
+    <SEO title="Project" />
+    <h1 className="red">Hi project</h1>
+  </Layout>
+)
+
+export default ProjectPage;
+
