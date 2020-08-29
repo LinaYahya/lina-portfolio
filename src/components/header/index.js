e72a3e63268a8c6f5391577c75e59a507fda81d3
Lina Ebeid <linaebe0@gmail.com>
2020-08-29 13:54:52 +0300

    create header component

---
+++ src/components/header/index.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { Link } from "gatsby"
+import ThemeToggle from "../themeToggle"
+import "./header.css"
+
+const Header = () => {
+ 
+  return (
+    <header className="nav-container">
+      <div className="nav-left">
+        <Link
+          className="nav"
+          style={{
+            color: "var(--textNormal)",
+          }}
+          to="/"
+          activeStyle={{ color: "var(--textLink)" }}
+        >
+          Home{" "}
+        </Link>
+        <Link
+          className="nav"
+          style={{
+            color: "var(--textNormal)",
+          }}
+          to="/project"
+          activeStyle={{ color: "var(--textLink)" }}
+        >
+          Project{" "}
+        </Link>
+        <Link
+          className="nav"
+          style={{
+            color: "var(--textNormal)",
+          }}
+          to="/about"
+          activeStyle={{ color: "var(--textLink)" }}
+        >
+          About{" "}
+        </Link>
+      </div>
+      <ThemeToggle className="nav-right" />
+    </header>
+  )
+}
+
+export default Header
+
+//      <svg class="blob-rotate-faster h-full text-fill-primary fill-current" viewBox="0 0 278 279" xmlns="http://www.w3.org/2000/svg"><path d="M137.896 0.127761C167.59 -0.638578 198.383 1.62824 222.877 18.4301C247.738 35.4836 263.129 63.014 271.706 91.9151C280.118 120.258 280.513 150.661 270.364 178.43C260.457 205.538 239.342 225.92 216.353 243.372C192.903 261.174 167.336 278.631 137.896 278.994C108.28 279.358 81.0666 263.928 58.0226 245.322C35.8955 227.455 20.5343 203.415 11.0775 176.594C1.41508 149.191 -4.23875 119.749 3.91245 91.8587C12.2111 63.4638 31.6331 39.4483 56.0438 22.7357C79.9856 6.34414 108.89 0.876363 137.896 0.127761Z"></path></svg>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,11 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import ThemeToggle from './themeToggle';
-
 import Header from "./header"
-// import "./layout.css"
-import '../../styles/global.css'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header  />
       <div
         style={{
      
@@ -40,7 +18,6 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <ThemeToggle />
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
