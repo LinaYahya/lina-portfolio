651ebaca3a74869bc5660447dc995df3b077bb6c
Lina Ebeid <linaebe0@gmail.com>
2020-08-30 23:38:16 +0300

    add project List as array

---
+++ src/data/projects.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    title: "ON THIS DAY",
+    description: "A project to discover what's happened in specific day, Who's born and who's died",
+    technologies: ['html', 'javascript'],
+    year: "2020",
+    github: "link1",
+    demo: "link2",
+  },
+  {
+    title: "Note App",
+    description: "Note App to add, edit, and delete your notes",
+    year: "2020",
+    technologies: ['NextJS', 'MongoDB'],
+    github: "https://github.com/LinaYahya/notes-nextJS",
+    demo: "link2",
+  },
+  {
+    title: "Quiz Game",
+    description: "A Quiz game from 10 questions the type of questions depends on the category and the difficulty you select",
+    year: "2020",
+    technologies: ['React hooks'],
+    github: "https://github.com/LinaYahya/quiz-game",
+    demo: "https://quiz-game1.netlify.app/",
+  },
+  {
+    title: "Room Booker",
+    description: "An app made for GSG that helps managing booked rooms.",
+    year: "2020",
+    technologies: ['React', 'Express','Postgres','fullCalendar'],
+
+    github: "https://github.com/GSG-G8/room-booker",
+    demo: "https://gsg-room-booker.herokuapp.com/",
+  },
+  {
+    title: "Art Gallery",
+    description: "A platform that connects Arabic painters with customers, and let artists have an account on this platform so they can publish their artworks and let users buy any product, with the ability to try painting on their walls as a virtual view.",
+    year: "2020",
+    technologies: ['React', 'Express','Postgres'],
+    github: "https://github.com/GSG-G8/art-gallery",
+    demo: "https://art-gallery-v1.herokuapp.com/",
+  },
+  
+]
