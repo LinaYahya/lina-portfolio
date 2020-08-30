ea15ce55229ebe1c47f33fad013f0b9ecf9003f0
Lina Ebeid <linaebe0@gmail.com>
2020-08-30 17:25:39 +0300

    move layout js and css files to layout folder

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,29 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Header from "./header"
+import Header from "../header"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Header  />
+      <Header />
       <div
         style={{
-     
-            backgroundColor: 'var(--bg)',
-            color: 'var(--textNormal)',
-            transition: 'color 0.2s ease-out, background 0.2s ease-out',
-        
+          backgroundColor: "var(--bg)",
+          color: "var(--textNormal)",
+          transition: "color 0.2s ease-out, background 0.2s ease-out",
           margin: `0 auto`,
-          maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer>
+        {/* <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </footer> */}
       </div>
     </>
   )
