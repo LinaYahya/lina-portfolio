d2b0706d15ba329609a8ebabc6cddaae1150aefd
Lina Ebeid <linaebe0@gmail.com>
2020-08-30 23:49:10 +0300

    initialize technologies component

---
+++ src/components/technologies.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+
+const TechnologiesList = () => {
+
+  return (
+    <div>
+      <h2>My Technology</h2>
+      <ul>
+        <li>tech 1</li>
+        <li>tech 2</li>
+        <li>tech 3</li>
+      </ul> 
+    </div>
+
+  )
+}
+
+export default TechnologiesList
