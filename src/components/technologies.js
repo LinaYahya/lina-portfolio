c4efa66ee63e02d31c88a397efa1716c86ab9b8e
Lina Ebeid <linaebe0@gmail.com>
2020-08-31 12:41:06 +0300

    render technologies and tools

--- src/components/technologies.js
+++ src/components/technologies.js
@@ -1,18 +1,12 @@
 import React from "react"
-
+import tools from "../images/tools.png"
 
 const TechnologiesList = () => {
-
   return (
     <div>
       <h2>My Technology</h2>
-      <ul>
-        <li>tech 1</li>
-        <li>tech 2</li>
-        <li>tech 3</li>
-      </ul> 
+      <img src={tools} alt="tools" />
     </div>
-
   )
 }
 
