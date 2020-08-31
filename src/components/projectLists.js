bc56ff978d72ae58b8f3d8ef5b8f9aef7cb84e64
Lina Ebeid <linaebe0@gmail.com>
2020-08-31 12:09:32 +0300

    add github and demo icon and change colors

--- src/components/projectLists.js
+++ src/components/projectLists.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import FontAwesome from 'react-fontawesome';
-import projects from '../data/projects';
-import './project.css'
+import React from "react"
+import projects from "../data/projects"
+import "./project.css"
 
 function ProjectList() {
   return (
     <>
       <h2>My Projects</h2>
       <ul className="project-list">
-        {projects.map(({ title, description,technologies, year, github, demo }, index) => (
-          <li className="project" key={index}>
-            <div style={{ width: '80%' }}>
-              <h3>{title}</h3>
-              <h4>{description}</h4>
-              {technologies?.map(e=> (
-                <span key={e}>{e}</span>
-              ))}
-            </div>
-            <div style={{ marginTop: '10%' }}>
-              <span>{year}</span>
-              <a href={github}>github</a>
-              <a href={demo}>demo</a>
-            </div>
-          </li>
-        ))
-        }
+        {projects.map(
+          ({ title, description, technologies, year, github, demo }, index) => (
+            <li className="project" key={index}>
+              <div style={{ width: "80%" }}>
+                <h3>{title}</h3>
+                <h4>{description}</h4>
+                {technologies?.map(e => (
+                  <span key={e}>{e}</span>
+                  ))}
+                <span style={{color:'var(--textLink)'}}>{year}</span>
+              </div>
+              <div style={{ marginTop: "10%" }}>
+                <a href={github}>
+                  <i class="fab fa-github"></i>
+                </a>
+                <a href={demo}>
+                  <i class="fas fa-link"></i>
+                </a>
+              </div>
+            </li>
+          )
+        )}
       </ul>
-
     </>
   )
 }
-export default ProjectList;
+export default ProjectList
